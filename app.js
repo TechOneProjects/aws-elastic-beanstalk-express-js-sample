969312330817325a1fbb571813ec763d5536c101
Ukpabia Ozor <ozorcard@Ukpabias-MacBook-Pro.local>
2024-05-17 16:29:21 -0400

    Full changes reviewed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("UK was here,You're welcome"));
+app.get('/', (req, res) => res.send("UK was here,You're welcome. Frankly my dear, I don't give a damn!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
